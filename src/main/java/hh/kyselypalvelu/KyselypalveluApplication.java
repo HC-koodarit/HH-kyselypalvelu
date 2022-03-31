922537739b0c006e33bc55f8e9038d043daf77a3
Sofia Lumme <sofia.lumme@gmail.com>
2022-03-31 10:08:38 +0300

    testikommentti

--- src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KyselypalveluApplication {
 
 	public static void main(String[] args) {
+		// näkyykö tämä?
 		SpringApplication.run(KyselypalveluApplication.class, args);
 	}
 
