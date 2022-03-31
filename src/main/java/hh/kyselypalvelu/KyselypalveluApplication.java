d93ec6ac5299adb74b1135051f8a48f62c750c69
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-03-31 10:55:04 +0300

    kyselyn lisays html

--- src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -9,11 +9,11 @@ public class KyselypalveluApplication {
 	public static void main(String[] args) {
 		// näkyykö tämä?
 		// näkyy
-<<<<<<< HEAD
+
 		// Marin testi
-=======
+
 		// näkyy joo
->>>>>>> ef97c88fb2a4d25076f744640696c5940f7b5bde
+
 		SpringApplication.run(KyselypalveluApplication.class, args);
 	}
 
