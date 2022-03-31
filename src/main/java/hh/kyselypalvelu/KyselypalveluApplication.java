70b66ac0ed019c8cc10216933f71a80f5dc39a5c
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-03-31 10:21:49 +0300

    marin testi

--- src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -9,6 +9,7 @@ public class KyselypalveluApplication {
 	public static void main(String[] args) {
 		// näkyykö tämä?
 		// näkyy
+		// Marin testi
 		SpringApplication.run(KyselypalveluApplication.class, args);
 	}
 
