a3700b582c1ab0bd14f0faa23fbadd39e678a04e
Sofia Lumme <sofia.lumme@gmail.com>
2022-04-05 14:18:23 +0300

    lisätty mahdollisuus katsoa kyselyiden kysymyksiä

--- src/main/java/hh/kyselypalvelu/domain/KysymysRepository.java
+++ src/main/java/hh/kyselypalvelu/domain/KysymysRepository.java
@@ -3,11 +3,12 @@ package hh.kyselypalvelu.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
 	
 	//List<Kysymys> findbyKysymys (String kysymysteksti);
-
+	
 }
--- src/main/java/hh/kyselypalvelu/web/KyselyController.java
+++ src/main/java/hh/kyselypalvelu/web/KyselyController.java
@@ -12,39 +12,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import hh.kyselypalvelu.domain.Kysely;
 import hh.kyselypalvelu.domain.KyselyRepository;
 import hh.kyselypalvelu.domain.Kysymys;
+import hh.kyselypalvelu.domain.KysymysRepository;
 
 @Controller
 public class KyselyController {
-	
+
 	@Autowired
 	private KyselyRepository kyselyRepository;
-	
+
+	@Autowired
+	private KysymysRepository kysymysRepository;
+
 	// näytä kaikki kyselyt
 	@GetMapping("/kyselylista")
 	public String getKyselyt(Model model) {
 		model.addAttribute("kyselyt", kyselyRepository.findAll());
 		return "kyselylista";
 	}
-	
+
 	// lisää uusi kysely
 	@RequestMapping("/kyselylista/add")
 	public String lisaaKysely(Model model) {
 		model.addAttribute("kysely", new Kysely());
 		return "lisaakysely";
 	}
-	
+
 	// tallenna kysely
 	@PostMapping("/kyselylista/save")
 	public String tallennaKysely(Kysely kysely) {
 		kyselyRepository.save(kysely);
 		return "redirect:/kyselylista";
 	}
-	
+
 	// muokkaa kyselyä
 	@RequestMapping("/kyselylista/edit/{id}")
 	public String muokkaaKyselya(@PathVariable("id") Long kyselyId, Model model) {
-		model.addAttribute("book", kyselyRepository.findById(kyselyId));
+		model.addAttribute("kysely", kyselyRepository.findById(kyselyId).get());
+		model.addAttribute("kysymykset", kyselyRepository.findById(kyselyId).get().getKysymykset());
 		return "lisaakysymyksia";
+	}
+
+	// lisää kysymys
+	@PostMapping("/kyselylista/edit/{id}/save")
+	public String tallennaKysely(@PathVariable("id") Long kyselyId, Kysymys kysymys) {
+		kysymysRepository.save(kysymys);
+		return "redirect:/kyselylista/edit/{id}";
 	}
 	
 }
+
