53572bcf10c45a2146989555bcb75357933a702f
SebuBergman <basti.bergman@gmail.com>
2022-04-06 14:13:29 +0300

    Crossorigin annotaatio lisäys herokuun

--- src/main/java/hh/kyselypalvelu/web/KyselyController.java
+++ src/main/java/hh/kyselypalvelu/web/KyselyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import hh.kyselypalvelu.domain.KyselyRepository;
 import hh.kyselypalvelu.domain.Kysymys;
 import hh.kyselypalvelu.domain.KysymysRepository;
 
+@CrossOrigin(origins = "http://domain2.com", maxAge = 3600)
 @Controller
 public class KyselyController {
 
