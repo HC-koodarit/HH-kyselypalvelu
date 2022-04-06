5a9032cb68d71d755e088d9dd854c53db10fc1ba
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-04-06 12:45:59 +0300

    kysymys rest

--- src/main/java/hh/kyselypalvelu/web/KysymysController.java
+++ src/main/java/hh/kyselypalvelu/web/KysymysController.java
@@ -1,5 +1,7 @@
 package hh.kyselypalvelu.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.kyselypalvelu.domain.Kysely;
 import hh.kyselypalvelu.domain.KyselyRepository;
 import hh.kyselypalvelu.domain.Kysymys;
 import hh.kyselypalvelu.domain.KysymysRepository;
@@ -30,5 +34,10 @@ public class KysymysController {
 	public String save(Kysymys kysymys) {
 		kysymysRepository.save(kysymys);
 		return "redirect:/_____";
+	}
+	
+	@GetMapping("/kysymykset")
+	public @ResponseBody List<Kysymys> kysymyslistaRest() {
+		return (List<Kysymys>) kysymysRepository.findAll();
 	}
 }
