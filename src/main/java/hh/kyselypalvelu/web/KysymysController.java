90238071a8e6fe3cb90c712b7b8d6a0c3aca4429
heuui <henni.haavistola@gmail.com>
2022-04-26 12:30:16 +0300

    muutoksia

--- src/main/java/hh/kyselypalvelu/web/KysymysController.java
+++ src/main/java/hh/kyselypalvelu/web/KysymysController.java
@@ -43,7 +43,7 @@ public class KysymysController {
 			return "kysymyslista";
 		}
 		
-		// KORVAA ALLA OLEVAN 
+		// KORVAA ALLA OLEVAN -> siirto KyselyControlleriin
 		// Avaa kysely -> esille muokkaa, lisää ja poistotoiminnot
 		@RequestMapping("/kysely/{id}")
 		public String avaaKysely(@PathVariable("id") Long kyselyId, Model model) {
@@ -101,10 +101,11 @@ public class KysymysController {
 			return "muokkaakyselya";	// muokkaakysymysta (?)
 		}
 		*/
-		
+		/*
 		// TODO: Tallenna muokattu kysymys
 		@PostMapping("/edit/{id}/save")
 		public String editKysymys(@PathVariable("id") Long kyselyId, Kysymys kysymys) {					kysymysRepository.save(kysymys);
 			return "redirect:/addkysymys/{id}"; // TODO: Muokkaa -> "redirect:/kysely/{id}"
 		}
+		*/
 }
