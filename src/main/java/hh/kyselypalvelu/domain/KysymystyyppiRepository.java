cbe369f3bab70241dcc7afff9403a17d6e240e11
Kristian Riihela <kristian.riihela@gmail.com>
2022-05-03 11:46:01 +0300

    uudet repot

---
+++ src/main/java/hh/kyselypalvelu/domain/KysymystyyppiRepository.java
@@ -0,0 +1,7 @@
+package hh.kyselypalvelu.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KysymystyyppiRepository extends CrudRepository<Kysymystyyppi, Long> {
+
+}
---
+++ src/main/java/hh/kyselypalvelu/domain/VaihtoehtoRepository.java
@@ -0,0 +1,7 @@
+package hh.kyselypalvelu.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VaihtoehtoRepository extends CrudRepository<Vaihtoehto, Long> {
+
+}
