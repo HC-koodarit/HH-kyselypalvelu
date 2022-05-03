5b05bb530715fd3aba7ce0e6ee006170392b98ed
Kristian Riihela <kristian.riihela@gmail.com>
2022-05-03 11:42:25 +0300

    uusia luokkia

---
+++ src/main/java/hh/kyselypalvelu/domain/Kysymystyyppi.java
@@ -0,0 +1,59 @@
+package hh.kyselypalvelu.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Kysymystyyppi {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nimi;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "kysymystyyppi")
+	private List<Kysymys> kysymykset;
+	
+	public Kysymystyyppi() {}
+
+	public Kysymystyyppi(String nimi, List<Kysymys> kysymykset) {
+		super();
+		this.nimi = nimi;
+		this.kysymykset = kysymykset;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public List<Kysymys> getKysymykset() {
+		return kysymykset;
+	}
+
+	public void setKysymykset(List<Kysymys> kysymykset) {
+		this.kysymykset = kysymykset;
+	}
+
+	@Override
+	public String toString() {
+		return "Kysymystyyppi [id=" + id + ", nimi=" + nimi + ", kysymykset=" + kysymykset + "]";
+	}
+
+}
---
+++ src/main/java/hh/kyselypalvelu/domain/Vaihtoehto.java
@@ -0,0 +1,59 @@
+package hh.kyselypalvelu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Vaihtoehto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nimi;
+	
+	@ManyToOne
+    @JoinColumn(name = "kysymysid")
+	private Kysymys kysymys;
+	
+	public Vaihtoehto() {}
+
+	public Vaihtoehto(String nimi, Kysymys kysymys) {
+		super();
+		this.nimi = nimi;
+		this.kysymys = kysymys;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public Kysymys getKysymys() {
+		return kysymys;
+	}
+
+	public void setKysymys(Kysymys kysymys) {
+		this.kysymys = kysymys;
+	}
+
+	@Override
+	public String toString() {
+		return "Vaihtoehto [id=" + id + ", nimi=" + nimi + ", kysymys=" + kysymys + "]";
+	}
+
+}
